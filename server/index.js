af4671537179d9f407617931aa333dd4f981e236
Yankhoba <bagayoko.yan@gmail.com>
2023-11-06 15:10:07 +0100

    Middleware POST requete

--- server/index.js
+++ server/index.js
@@ -1,11 +1,15 @@
 // Ici on charge express afin de pouvoir s'en servire dans notre application
-const express = require("express")
+const express = require("express");
 
-// Ici on met express dans une constante app 
+// Ici on met express dans une constante app
 const app = express();
-
+const PORT = 5001;
+    // Middleware pour gérer les requetes POST
+    app.use(express.urlencoded({extended:true}))
 // ici on créer un route pour affcher un message simple simple
-app.post('/api/save', (req, res) =>{
-    res.send('Build something amazing! 🚀')
-})
-app.listen(5000,() => console.log('Le serveur est lancé sur le port 5000'));
+app.post("/api/save", (req, res) => {
+    const imdbID = req.body.imdbID
+  res.send("Build something amazing! 🚀");
+  console.log(imdbID);
+});
+app.listen(PORT, () => console.log("Le serveur est lancé sur le port " + PORT));
