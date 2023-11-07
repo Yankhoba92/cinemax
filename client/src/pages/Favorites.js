141ff7d4b6ce9b92daa56bfb0c34df66d07027c2
Yankhoba <bagayoko.yan@gmail.com>
2023-11-07 16:54:17 +0100

    🚧 Affichage des données dans notre favrites

--- client/src/pages/Favorites.js
+++ client/src/pages/Favorites.js
@@ -1,13 +1,25 @@
 import React from "react";
+import { useEffect, useState } from "react";
 
-// Cec composant représenete la barre de recherche
-const Favorites = ({ Favorites }) => {
-  // Lien IMDB
-  const imdb = "https://www.imdb.com/title/";
+const Favorites = ({}) => {
+  const [favorites, setFavorites] = useState([]);
+
+  const apiJson = "http://localhost:5001/api/favorites";
+  useEffect(() => {
+    fetch(apiJson)
+      .then((response) => response.json())
+      .then((data) => setFavorites(data.favorites)); // On ajoute les favoris à la constante favorites
+  });
 
   return (
     <>
-    <h1>Favorites</h1>
+      <h1>Favorites</h1>
+      <ul>
+        {favorites.map((favorite) => (
+          <li key={favorite.id}>{favorite.movie}</li>
+        ))
+        }
+      </ul>
     </>
   );
 };
