23c591eb708f1f04099a6332c6af1d5850fee776
Yankhoba <bagayoko.yan@gmail.com>
2023-11-08 11:48:42 +0100

    🚧  récupétarion des datas avec forEach

--- client/src/pages/Favorites.js
+++ client/src/pages/Favorites.js
@@ -3,22 +3,27 @@ import { useEffect, useState } from "react";
 
 const Favorites = ({}) => {
   const [favorites, setFavorites] = useState([]);
+  const [movies, setMovies] = useState([]);
 
-  const apiJson = "http://localhost:5001/api/favorites";
+  const API = "http://www.omdbapi.com/?apikey=78620d47";
   useEffect(() => {
-    fetch(apiJson)
+    fetch("http://localhost:5001/api/favorites")
       .then((response) => response.json())
       .then((data) => setFavorites(data.favorites)); // On ajoute les favoris à la constante favorites
   }, []);
-
+  favorites.forEach((item) => {
+    fetch(API + "&i="+ item.movie)
+    .then(res => res.json())
+    .then(data => console.log(data))
+  });
   return (
     <>
       <h1>Favorites</h1>
       <ul>
+        {/* {console.log(favorites)} */}
         {favorites.map((favorite) => (
           <li key={favorite.id}>{favorite.movie}</li>
-        ))
-        }
+        ))}
       </ul>
     </>
   );
